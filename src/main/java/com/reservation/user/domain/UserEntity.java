2bb418cbaf5cf0e6314ded1c25bfd91aa86217cd
CopyPaste <sjnnj@naver.com>
2024-11-02 16:26:10 +0900

    feat: add UserEntity

--- src/main/java/com/reservation/user/domain/UserEntity.java
+++ src/main/java/com/reservation/user/domain/UserEntity.java
@@ -1,5 +1,6 @@
 package com.reservation.user.domain;
 
+import com.reservation.global.audit.MutableBaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,10 +10,24 @@ import lombok.NoArgsConstructor;
 @Table(name = "user")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserEntity {
+public class UserEntity extends MutableBaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id") private Long userId;
 
-    @Column private String firstName;
+    @Column(name = "user_name") private String username;
+
+    @Column(name = "provider") private String provider;
+
+    @Column(name = "provider_id") private String providerId;
+
+    @Column(name = "email") private String email;
+
+    @Column(name = "phone") private String phone;
+
+    public UserEntity(String username, String provider, String providerId) {
+        this.username = username;
+        this.provider = provider;
+        this.providerId = providerId;
+    }
 }
